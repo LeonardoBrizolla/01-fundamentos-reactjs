8d0e7df5342c0d8cef3b2ece3a0ef60d9767fc98
Leonardo Brizolla <leonardobrizollars@gmail.com>
2022-06-22 16:51:43 -0300

    Entendendo a key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,9 +9,9 @@ import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState([
-    1,
-    2,
+    'Post muito bacana, hein?!'
   ]);
+  const [newCommentText, setNewCommentText] = useState('');
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
@@ -24,8 +24,14 @@ export function Post({ author, publishedAt, content }) {
 
   function handleCreateNewComment() {
     event.preventDefault();
-    setComments([...comments, comments.length + 1]);
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
   };
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
+  }
   
   return (
     <article className={styles.post}>
@@ -51,10 +57,10 @@ export function Post({ author, publishedAt, content }) {
 
         {content.map(line => {
             if (line.type === 'paragraph') {
-              return <p>{line.content}</p>
+              return <p key={line.content}>{line.content}</p>
             }
             if (line.type === 'link') {
-              return <p><a href="#">{line.content}</a></p>
+              return <p key={line.content}><a href="#">{line.content}</a></p>
             }
             return <p>Conteúdo não encontrado</p>
         })}
@@ -63,8 +69,11 @@ export function Post({ author, publishedAt, content }) {
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea 
+        <textarea
+          name='comment'
           placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -74,7 +83,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment />
+          return <Comment key={comment} content={comment} />
         })}
       </div>
     </article>
